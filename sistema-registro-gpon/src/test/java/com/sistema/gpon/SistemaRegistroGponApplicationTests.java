e5477def4300c8a50c111020f166671ddf4284f0
jason-vila <jasonvilac@gmail.com>
2025-06-17 19:38:37 -0500

    Agregar estructura inicial del proyecto

---
+++ sistema-registro-gpon/src/test/java/com/sistema/gpon/SistemaRegistroGponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistema.gpon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaRegistroGponApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
