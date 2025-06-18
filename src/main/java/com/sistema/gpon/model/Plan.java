400af8c2bfff2a5838df90d5cdd5bc880ad9843b
jason-vila <jasonvilac@gmail.com>
2025-06-17 20:15:44 -0500

    Crear modelos de entidad con JPA y Lombok

---
+++ src/main/java/com/sistema/gpon/model/Plan.java
@@ -0,0 +1,25 @@
+package com.sistema.gpon.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "Plan")
+@Data
+@NoArgsConstructor
+public class Plan {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "IdPlan")
+	private Integer idPlan;
+
+	@Column(name = "Descripcion")
+	private String descripcion;
+}
---
+++ src/main/java/com/sistema/gpon/model/Usuario.java
@@ -0,0 +1,40 @@
+package com.sistema.gpon.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "Usuario")
+@Data
+@NoArgsConstructor
+public class Usuario {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "IdUsuario")
+	private Integer idUsuario;
+
+	@ManyToOne
+	@JoinColumn(name = "IdRol")
+	private Rol rol;
+
+	@Column(name = "Nombre")
+	private String nombre;
+
+	@Column(name = "Apellido")
+	private String apellido;
+
+	@Column(name = "Correo")
+	private String correo;
+
+	@Column(name = "Contrasena")
+	private String contrasena;
+}
