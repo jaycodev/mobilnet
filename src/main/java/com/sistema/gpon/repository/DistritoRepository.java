9fb76213e58951ac9df1f179d36c8ad4a7e235f9
jason-vila <jasonvilac@gmail.com>
2025-06-17 21:19:18 -0500

    Agregar repositories para todas las entidades JPA

---
+++ src/main/java/com/sistema/gpon/repository/DistritoRepository.java
@@ -0,0 +1,8 @@
+package com.sistema.gpon.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.sistema.gpon.model.Distrito;
+
+public interface DistritoRepository extends JpaRepository<Distrito, Integer> {
+}
---
+++ src/main/java/com/sistema/gpon/repository/RolRepository.java
@@ -0,0 +1,8 @@
+package com.sistema.gpon.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.sistema.gpon.model.Rol;
+
+public interface RolRepository extends JpaRepository<Rol, Integer> {
+}
