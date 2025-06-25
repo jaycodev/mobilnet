e3cbfa1d47d30084b5fc7a25911b0b3d099b7084
jose angel <joseangelespinozamorales@gmail.com>
2025-06-24 20:11:40 -0500

    Uso de Layout

--- src/main/java/com/sistema/gpon/controller/PlanController.java
+++ src/main/java/com/sistema/gpon/controller/PlanController.java
@@ -2,6 +2,8 @@ package com.sistema.gpon.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.sistema.gpon.service.PlanService;
@@ -12,4 +14,9 @@ public class PlanController {
 
     @Autowired
     private PlanService planService;
+    @GetMapping("/index")
+    public String formGpon(Model model){
+
+        return "planes/index";
+    }
 }
--- src/main/java/com/sistema/gpon/controller/RegistroRUC10Controller.java
+++ src/main/java/com/sistema/gpon/controller/RegistroRUC10Controller.java
@@ -2,14 +2,22 @@ package com.sistema.gpon.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.sistema.gpon.service.RegistroRUC10Service;
 
 @Controller
-@RequestMapping("/registros")
+@RequestMapping("/mobinet")
 public class RegistroRUC10Controller {
 
     @Autowired
     private RegistroRUC10Service registroRUC10Service;
+
+    @GetMapping("/formGpon")
+    public String formGpon(Model model){
+
+        return "formGeneral/formGpon";
+    }
 }
