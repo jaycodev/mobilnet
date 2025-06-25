ad1b788f42a87bcdab77eeffaa281b9a7e1a8e94
jason-vila <jasonvilac@gmail.com>
2025-06-25 13:05:11 -0500

    🎨 style: fix layout and prepare base for additional views

--- src/main/java/com/sistema/gpon/controller/ClienteController.java
+++ src/main/java/com/sistema/gpon/controller/ClienteController.java
@@ -1,7 +1,10 @@
 package com.sistema.gpon.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.sistema.gpon.service.ClienteService;
@@ -12,4 +15,10 @@ public class ClienteController {
 
     @Autowired
     private ClienteService clienteService;
+
+    @GetMapping({"", "/"})
+    public String index(HttpServletRequest request, Model model) {
+        model.addAttribute("uri", request.getRequestURI());
+        return "clientes/index";
+    }
 }
