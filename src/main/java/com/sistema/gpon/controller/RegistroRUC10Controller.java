f0b05b2b560be46ceed7ba66eb6b47ff5ba6297e
jason-vila <jasonvilac@gmail.com>
2025-06-25 19:27:48 -0500

    🔧 chore: resolve merge conflicts on branch 'angel'

--- src/main/java/com/sistema/gpon/controller/RegistroRUC10Controller.java
+++ src/main/java/com/sistema/gpon/controller/RegistroRUC10Controller.java
@@ -1,9 +1,9 @@
 package com.sistema.gpon.controller;
 
-import com.sistema.gpon.service.UsuarioService;
 import com.sistema.gpon.service.impl.PlanServiceImpl;
 import com.sistema.gpon.service.impl.PromocionServiceImpl;
 import com.sistema.gpon.service.impl.UsuarioServiceImpl;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,30 +12,31 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.sistema.gpon.service.RegistroRUC10Service;
 
-import java.awt.color.ICC_Profile;
-
 @Controller
-@RequestMapping("/formGeneral")
+@RequestMapping("/registros")
 public class RegistroRUC10Controller {
 
     @Autowired
     private RegistroRUC10Service registroRUC10Service;
 
     @Autowired
-    private UsuarioServiceImpl _UsuarioService;
+    private UsuarioServiceImpl usuarioService;
 
     @Autowired
-    private PromocionServiceImpl _PromocionService;
+    private PromocionServiceImpl promocionService;
 
     @Autowired
-    private PlanServiceImpl _PlanService;
+    private PlanServiceImpl planService;
 
-    @GetMapping("/formGpon")
-    public String formGpon(Model model){
-        model.addAttribute("Consultor",_UsuarioService.findByRol_Descripcion("Consultor"));
-        model.addAttribute("Supervisor",_UsuarioService.findByRol_Descripcion("Supervisor"));
-        model.addAttribute("promocion", _PromocionService.listarPromociones());
-        model.addAttribute("plam", _PlanService.listarTodoPlanes());
-        return "formGeneral/formGpon";
+    @GetMapping({"", "/"})
+    public String index(HttpServletRequest request, Model model) {
+        model.addAttribute("uri", request.getRequestURI());
+
+        model.addAttribute("Consultor", usuarioService.findByRol_Descripcion("Consultor"));
+        model.addAttribute("Supervisor", usuarioService.findByRol_Descripcion("Supervisor"));
+        model.addAttribute("promocion", promocionService.listarPromociones());
+        model.addAttribute("plam", planService.listarTodoPlanes());
+
+        return "registros/index";
     }
 }
