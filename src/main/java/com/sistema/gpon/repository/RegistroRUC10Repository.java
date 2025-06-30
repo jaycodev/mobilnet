003437eca6178f584b74f5c6c2a600052a52eb7c
loverkity121 <loverkity121@gmail.com>
2025-06-30 18:01:36 -0500

    CRUD REGISTRO COMPLETO

--- src/main/java/com/sistema/gpon/repository/RegistroRUC10Repository.java
+++ src/main/java/com/sistema/gpon/repository/RegistroRUC10Repository.java
@@ -5,6 +5,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.sistema.gpon.model.RegistroRUC10;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface RegistroRUC10Repository extends JpaRepository<RegistroRUC10, Integer> {
+    List<RegistroRUC10> findByActivo(int activo);
 }
